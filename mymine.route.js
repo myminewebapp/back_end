5d621aad3ef18838f9b0982b8fc182d220a1da7a
Pakorn Chiraksa <pakonchiraksa@gmail.com>
2019-05-31 18:23:02 +0700

    add auth req for secure back end

--- mymine.route.js
+++ mymine.route.js
@@ -8,6 +8,7 @@ let mymineModels = require('./mymine.model');
 let { AccountModel, MemoryModel } = mymineModels;
 
 const CODE = require('./mymine.res.code');
+const withAuth = require('./middleware');
 
 let resJsonGen = (resMsg, code) => {
   return {
@@ -17,89 +18,107 @@ let resJsonGen = (resMsg, code) => {
 };
 
 //get timelines by account id
-mymineRoutes.route('/timeline/:id').get(function (req, res) {
+mymineRoutes.route('/timeline/:id').get(withAuth, function (req, res) {
   if(!ObjectId.isValid(req.params.id)){
     res.json(resJsonGen('Invalid id pattern.',CODE.error.objectIdInvalid));
   }
   else{
     let accountID = new ObjectId(req.params.id);
-    MemoryModel.find({owner_account: accountID, is_delete: false}).sort('-date').limit(3).exec(function (err, result) {
-      if (err) {
-        res.json(resJsonGen(err,CODE.error.other));
+    let email = req.email;
+    AccountModel.findOne({ email }, function(err, account) {
+      if (!err) {
+        if(account){
+          if(accountID.toString() === account._id.toString()){
+            MemoryModel.find({owner_account: accountID, is_delete: false}).sort('-date').limit(3).exec(function (err, result) {
+              if (err) {
+                res.json(resJsonGen(err,CODE.error.other));
+              }else{
+                res.status(200).json(result);
+              }
+            });
+            // .populate('owner_account').exec((err, account) => {
+            //   console.log(account);
+            // });
+          }else{
+            res.status(401).send("Unauthorized: token doesn't access provided");
+          }
+        }else{
+          res.status(401).send("Unauthorized: token doesn't access provided");
+        }
       }else{
-        res.json(result);
-      }
+        res.json(resJsonGen(err,CODE.error.other));
+      }     
     });
-    // .populate('owner_account').exec((err, account) => {
-    //   console.log(account);
-    // });
   }
 });
 
 //get all memories by account id
-mymineRoutes.route('/memory/:id').get(function (req, res) {
+mymineRoutes.route('/memory/:id').get(withAuth, function (req, res) {
   if(!ObjectId.isValid(req.params.id)){
     res.json(resJsonGen('Invalid id pattern.',CODE.error.objectIdInvalid));
   }
   else{
     let accountID = new ObjectId(req.params.id);
-    MemoryModel.find({owner_account: accountID, is_delete: false}).sort('-date').exec(function (err, result) {
-      if (err) {
-        res.json(resJsonGen(err,CODE.error.other));
+    let email = req.email;
+    AccountModel.findOne({ email }, function(err, account) {
+      if (!err) {
+        if(account){
+          if(accountID.toString() === account._id.toString()){
+            MemoryModel.find({owner_account: accountID, is_delete: false}).sort('-date').exec(function (err, result) {
+              if (err) {
+                res.json(resJsonGen(err,CODE.error.other));
+              }else{
+                res.status(200).json(result);
+              }
+            });
+            // .populate('owner_account').exec((err, account) => {
+            //   console.log(account);
+            // });
+          }else{
+            res.status(401).send("Unauthorized: token doesn't access provided");
+          }
+        }else{
+          res.status(401).send("Unauthorized: token doesn't access provided");
+        }
       }else{
-        res.json(result);
-      }
-    });
-    // .populate('owner_account').exec((err, account) => {
-    //   console.log(account);
-    // });
+        res.json(resJsonGen(err,CODE.error.other));
+      }     
+    });   
   }
 });
 
 //get all delete memories by account id
-mymineRoutes.route('/memory/delete/:id').get(function (req, res) {
+mymineRoutes.route('/memory/delete/:id').get(withAuth, function (req, res) {
   if(!ObjectId.isValid(req.params.id)){
     res.json(resJsonGen('Invalid id pattern.',CODE.error.objectIdInvalid));
   }
   else{
     let accountID = new ObjectId(req.params.id);
-    MemoryModel.find({owner_account: accountID,is_delete: true}).sort('-date').exec(function (err, result) {
-      if (err) {
-        res.json(resJsonGen(err,CODE.error.other));
+    let email = req.email;
+    AccountModel.findOne({ email }, function(err, account) {
+      if (!err) {
+        if(account){
+          if(accountID.toString() === account._id.toString()){
+            MemoryModel.find({owner_account: accountID,is_delete: true}).sort('-date').exec(function (err, result) {
+              if (err) {
+                res.json(resJsonGen(err,CODE.error.other));
+              }else{
+                res.status(200).json(result);
+              }
+            });
+            // .populate('owner_account').exec((err, account) => {
+            //   console.log(account);
+            // });
+          }else{
+            res.status(401).send("Unauthorized: token doesn't access provided");
+          }
+        }else{
+          res.status(401).send("Unauthorized: token doesn't access provided");
+        }
       }else{
-        res.json(result);
-      }
-    });
-    // .populate('owner_account').exec((err, account) => {
-    //   console.log(account);
-    // });
-  }
-});
-
-// mymineRoutes.route('/memoryAll').get(function (req, res) {
-//   MemoryModel.find((err, result) => {
-//     if (err) {
-//       res.json(resJsonGen(err,CODE.error.other));
-//     }else{
-//       res.json(result);
-//     }
-//   });
-// });
-
-//get profile data by account id
-mymineRoutes.route('/profile/:id').get(function (req, res) {
-  if(!ObjectId.isValid(req.params.id)){
-    res.json(resJsonGen('Invalid id pattern.',CODE.error.objectIdInvalid));
-  }
-  else{
-    let accountID = new ObjectId(req.params.id);
-    AccountModel.findById(accountID, function (err, result) {
-      if (err) {
         res.json(resJsonGen(err,CODE.error.other));
-      }else{
-        res.json(result);
-      }
-    });
+      }     
+    });   
   }
 });
 
@@ -122,14 +141,14 @@ mymineRoutes.route('/register').post(function (req, res) {
         }
       }else{
         // res.json(result);
-        res.json(resJsonGen("Register complete!!",CODE.normal.regisComplete));
+        res.status(200).json(resJsonGen("Register complete!!",CODE.normal.regisComplete));
       }
     });
   }
 });
 
 //create memory
-mymineRoutes.route('/memory/').post(function (req, res) {
+mymineRoutes.route('/memory/').post(withAuth, function (req, res) {
   let {owner_account, message_data, emojiValue} = req.body;
   if((owner_account === "" || owner_account === undefined) || 
      (message_data === "" || message_data === undefined) || 
@@ -155,7 +174,7 @@ mymineRoutes.route('/memory/').post(function (req, res) {
               res.json(resJsonGen(err,CODE.error.other));
             }else{
               // res.json(result);
-              res.json(resJsonGen("Create memory success",CODE.normal.createMemorySuccess));
+              res.status(200).json(resJsonGen("Create memory success",CODE.normal.createMemorySuccess));
             }
           });
         }
@@ -165,18 +184,34 @@ mymineRoutes.route('/memory/').post(function (req, res) {
 });
 
 //delete memory (isDelete = true)
-mymineRoutes.route('/memory/:id').delete(function (req, res) {
+mymineRoutes.route('/memory/:id').delete(withAuth, function (req, res) {
   let id = req.params.id;
   if(!ObjectId.isValid(id)){
     res.json(resJsonGen('Invalid id pattern.',CODE.error.objectIdInvalid));
   }
   else{
-    MemoryModel.findByIdAndUpdate(id, {is_delete : true}, function (err, result) {
-      if (err) {
-        res.json(resJsonGen(err,CODE.error.other));
+    let memoryID = new ObjectId(id);
+    let email = req.email;
+    AccountModel.findOne({ email }, function(err, account) {
+      if (!err) {
+        if(account){
+          MemoryModel.findOneAndUpdate({_id: memoryID , owner_account: account._id}, {is_delete : true}, function (err, result) {
+            if (err) {
+              res.json(resJsonGen(err,CODE.error.other));
+            }else{
+              if(result !== null){
+                res.status(200).json(result);
+              }else{
+                res.status(404).send("Error: No match memory_id or you aren't owner this memory");
+              }
+            }
+          });
+        }else{
+          res.status(401).send("Unauthorized: token doesn't access provided");
+        }
       }else{
-        res.json(result);
-      }
+        res.json(resJsonGen(err,CODE.error.other));
+      }     
     });
   }
 });
